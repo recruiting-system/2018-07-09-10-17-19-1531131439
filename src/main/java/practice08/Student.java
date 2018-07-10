e5f9ee7ea163bb6c9d7c1c644a7ba2a30152db26
louis <louis.z.j.li@oocl.com>
2018-07-10 15:28:35 +0800

    [louis] complete pratice-8

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,30 @@
 package practice08;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+
+
+    @Override
+    public String introduce() {
+        if (klass != klass.getLeader().getKlass()) {
+            return super.introduce()+" I am a Student. I am Leader of Class "+klass.getNumber()+".";
+        } else {
+            return "";
+        }
+
+    }
 }
