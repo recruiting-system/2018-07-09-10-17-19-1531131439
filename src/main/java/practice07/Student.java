3e4169481f8bdf3bb29629d675c86a8fbd8fda30
louis <louis.z.j.li@oocl.com>
2018-07-10 15:01:35 +0800

    [louis] complete pratice-7

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,25 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Student(String name, int age, Klass kclass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am at Class "+this.klass.getNumber()+".";
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,45 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+    @Override
+    public String introduce() {
+        String temString = " I teach No Class.";
+        if(klass.getNumber()!=0){
+            temString = " I teach Class "+klass.getNumber()+".";
+        }
+        return super.introduce()+" I am a Teacher." +temString;
+    }
+    public String introduceWith(Student student) {
+        String temString = " I don't teach "+student.getName()+".";
+        if(this.equal(student.getKlass())){
+            temString = " I teach "+student.getName()+".";
+        }
+        return "My name is "+getName()+". I am "+getAge()+" years old. I am a Teacher."+temString+"";
+    }
+    public boolean equal (Klass klass){
+        if(getKlass().getNumber() == klass.getNumber()){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
